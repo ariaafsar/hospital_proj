3a52f880ac9747fa9c7ebd030879008561c57498
aria <aria.legion2@gmail.com>
2024-09-06 11:36:33 +0330

    core models

--- hospital/core/models.py
+++ hospital/core/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class doctor(models.Model):
+    user = models.OneToOneField(User , on_delete= models.PROTECT)
+    category = models.charField(max_length=100)
+
+class patient(models.Model):
+    user = models.OneToOneField(User , on_delete= models.PROTECT)
+    doctor = models.ForeignKey(doctor, on_delete=models.CASCADE)
+
+class service(models.Model) :
+    date = models.DateField()
+    type = models.CharField(max_length=100)
+    doctor = models.ForeignKey(doctor, on_delete=models.CASCADE)
+    patient = models.ForeignKey(patient, on_delete=models.CASCADE)
+    price = models.IntegerField()
 
 # Create your models here.
