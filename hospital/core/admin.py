8aad0633f43f2e2e2bb6d8a85a946591932a7c30
Alireza Shams <alerzs1023@gmail.com>
2024-09-06 12:34:28 +0330

    fwefeg

--- hospital/core/admin.py
+++ hospital/core/admin.py
@@ -1,3 +1,37 @@
-from django.contrib import admin
+from django.contrib.admin import register, ModelAdmin
+from .models import Doctor , Patient , Service , Trans
 
-# Register your models here.
+
+@register(Doctor)
+class DoctorAdmin(ModelAdmin):
+    list_display = [
+        'user',
+        'category'
+    ]
+
+
+@register(Patient)
+class PatientAdmin(ModelAdmin):
+    list_display = [
+        'user',
+    ] 
+
+
+
+@register(Service)
+class ServiceAdmin(ModelAdmin):
+    list_display = [
+        'date',
+        'type',
+        'doctor',
+        'patient',
+        'price',
+        'status',
+    ] 
+
+@register(Trans)
+class TransAdmin(ModelAdmin):
+    list_display = [
+        'service',
+        'date'
+    ] 
